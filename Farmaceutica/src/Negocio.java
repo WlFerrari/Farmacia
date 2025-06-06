7b0684a202105466e873e7b5699d38829c1320b3
Ferrari <vitorferrarijoao5@gmail.com>
2025-06-06 14:43:54 -0300

    Adicionando os Enums (Tipo e Status) e Classes Negocio e Transportadora

---
+++ Farmaceutica/src/Negocio.java
@@ -0,0 +1,53 @@
+import java.util.UUID;
+
+public class Negocio {
+    private UUID id;
+    private Tipo tipo;
+    private Status status;
+    private List<Funcionario> participantes;
+    private Transportadora transportadora;
+
+    public Negocio(Tipo tipo, Status status, List<Funcionario> participantes, Transportadora transportadora) {
+        this.id = UUID.randomUUID(); // Gerar um id aleatórios único
+        this.tipo = tipo;
+        this.status = status;
+        this.participantes = participantes;
+        this.transportadora = transportadora;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<Funcionario> getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(List<Funcionario> participantes) {
+        this.participantes = participantes;
+    }
+
+    public Transportadora getTransportadora() {
+        return transportadora;
+    }
+
+    public void setTransportadora(Transportadora transportadora) {
+        this.transportadora = transportadora;
+    }
+}
