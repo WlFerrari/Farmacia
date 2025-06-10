3a2fdd65a5b51c8643043f8339eda6ad7ae5d632
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2025-06-10 12:19:41 -0300

    atributo de ativo adicionado as classes

--- Farmaceutica/src/Transportadora.java
+++ Farmaceutica/src/Transportadora.java
@@ -3,27 +3,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Transportadora {
+    private static int counter = 0;
+    private final int id;
     private String nome;
     private List<String> regioes;
-    private final int id;
-    private static int counter = 0;
+    private boolean isAtivo;
 
     public Transportadora(String nome, List<String> regioes) {
         this.nome = nome;
         this.regioes = regioes;
         this.id = ++counter;
-    }
-
-    // Getters e Setters
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public List<String> getRegioes() { return regioes; }
-    public void setRegioes(List<String> regioes) { this.regioes = regioes; }
-    public int getId() { return id; }
-
-    @Override
-    public String toString() {
-        return "ID: " + id + "\nTransportadora: " + nome + "\nRegiões de cobertura: " + String.join(", ", regioes);
+        isAtivo = true;
     }
 
     // ATUALIZADO: Recebe o Scanner como parâmetro
@@ -52,8 +42,8 @@ public class Transportadora {
         }
 
         System.out.println("Selecione a transportadora para editar:");
-        for(int i = 0; i < lista.size(); i++) {
-            System.out.printf("%d. %s%n", (i+1), lista.get(i).getNome());
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.printf("%d. %s%n", (i + 1), lista.get(i).getNome());
         }
         System.out.print("Opção: ");
         int indice = Integer.parseInt(sc.nextLine()) - 1;
@@ -70,5 +60,39 @@ public class Transportadora {
             transportadora.setNome(novoNome);
         }
         System.out.println("Transportadora atualizada com sucesso!");
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<String> getRegioes() {
+        return regioes;
+    }
+
+    public void setRegioes(List<String> regioes) {
+        this.regioes = regioes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAtivo() {
+        return isAtivo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        isAtivo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nTransportadora: " + nome + "\nRegiões de cobertura: " + String.join(", ", regioes);
     }
 }
